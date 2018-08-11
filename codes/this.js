9c6392843266c742a9de352b93c7882f7c246dcf
Nayan <nayansingh1209@gmail.com>
2018-08-11 17:22:17 +0530

    this updated

--- codes/this.js
+++ codes/this.js
@@ -1,13 +1,16 @@
-// this is object owner of the function
+// this refers to object owner of the function
 
-var firstName="nayan";
-var lastName=" singh";
+var firstName="nayan ";
+var lastName=" singh ";
 
 console.log(this);                          // it will return window in google console
 
 function fullName(){
+    var firstName="naman ";
+    var lastName="rajvanshi ";
     // return this;                          // it will return the global object
-    // return firstName+lastName;            // it will return global object key values    
+    return firstName+lastName;            // it will return global object key values   
+     // return this.firstName+this.lastName;  // it will return global 
 }
 
 console.log(fullName());
